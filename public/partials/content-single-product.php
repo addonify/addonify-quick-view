e282788a0ada93fa7cb79e1f717d294f6782a677
Sanil Shakya <sonill_ss@hotmail.com>
2020-07-29 15:43:29 +0545

    regular updates

---
+++ public/partials/content-single-product.php
@@ -0,0 +1,28 @@
+<?php
+
+
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+/**
+ * Hook: woocommerce_before_single_product.
+ *
+ * @hooked wc_print_notices - 10
+ */
+do_action( 'woocommerce_before_single_product' );
+
+if ( post_password_required() ) {
+	echo get_the_password_form(); // WPCS: XSS ok.
+	return;
+}
+?>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+
+	<?php do_action( 'woocommerce_before_single_product_summary' ); ?>
+
+	<div class="summary entry-summary">
+		<?php do_action( 'woocommerce_single_product_summary' ); ?>
+	</div>
+
+</div>
