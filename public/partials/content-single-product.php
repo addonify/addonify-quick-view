e0aaecb7372dc3d17e281dba5d6c9c7374444988
Sanil Shakya <sonill_ss@hotmail.com>
2020-08-01 21:14:25 +0545

    code cleanups

--- public/partials/content-single-product.php
+++ public/partials/content-single-product.php
@@ -5,7 +5,6 @@
 		the_post();
 ?>
 
-<?php woocommerce_show_product_images();  ?>
 	<div class="woocommerce single-product">
 		<div id="product-<?php the_ID(); ?>" <?php post_class( 'product' ); ?>>
 			<?php do_action( 'addonify_qv_product_image' ); ?>
