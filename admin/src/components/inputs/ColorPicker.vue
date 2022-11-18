aa62d7abeeb46caf3b911e202e1758863fbd563e
h1dd3n_sn1p3r <struggleage@gmail.com>
2022-11-18 12:59:48 +0545

    👌 Improvement: ColorPicker.
    👌 Improvement: ColorPicker.

--- admin/src/components/inputs/ColorPicker.vue
+++ admin/src/components/inputs/ColorPicker.vue
@@ -3,7 +3,7 @@
 	import { ElColorPicker } from "element-plus";
 	const props = defineProps({
 		colorVal: String,
-		isAlpha: Boolean,
+		isAlpha: [Boolean, String],
 		label: String,
 	});
 	const emit = defineEmits(["update:colorVal"]); // Ref: https://vuejs.org/guide/components/events.html#usage-with-v-model
@@ -15,9 +15,18 @@
 			emit("update:colorVal", newValue);
 		},
 	});
+
+	const handleColorChanged = (color) => {
+		// Emit the color immediately. Don't wait till the "ok" button is clicked.
+		emit("update:colorVal", color);
+	};
 </script>
 <template>
-	<el-color-picker v-model="value" :show-alpha="props.isAlpha" />
+	<el-color-picker
+		v-model="value"
+		:show-alpha="props.isAlpha"
+		@active-change="handleColorChanged"
+	/>
 	<span class="label" v-if="props.label">{{ props.label }}</span>
 </template>
 <style>
@@ -25,14 +34,26 @@
 	.adfy-options .el-color-picker__color,
 	.adfy-options .el-color-picker__color-inner {
 		border-radius: 100%;
-	}
-	.adfy-options .el-color-picker__color {
 		border: none;
 	}
+
 	.adfy-options .el-color-picker__trigger {
-		height: 36px;
-		width: 36px;
-		padding: 5px;
-		border: 2px dotted #bbbbbb;
+		height: 42px;
+		width: 42px;
+		padding: 3px;
+		border: 2px solid white;
+		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+	}
+
+	.adfy-options .el-color-picker .el-color-picker__icon {
+		font-size: 16px;
+		color: white;
+		line-height: 1;
+	}
+
+	.adfy-options .el-color-picker .el-color-picker__empty {
+		font-size: 20px;
+		color: red;
+		line-height: 1;
 	}
 </style>
