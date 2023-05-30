e1877189d10a86a9ab1aa9aab4e33905b2724b19
Anuj Subedi <struggleage@gmail.com>
2023-05-30 13:19:09 +0545

    Tweak: How reactive state on plugin setting's page is managed (vue js).
    -  Tweak: How reactive state on plugin setting's page is managed (vue js).

--- admin/src/stores/options.js
+++ admin/src/stores/options.js
@@ -3,6 +3,7 @@ const { isEqual, cloneDeep } = lodash;
 const { apiFetch } = wp;
 import { ElMessage } from 'element-plus'
 
+const { __ } = wp.i18n;
 const BASE_API_URL = adfy_wp_locolizer.rest_namespace;
 let oldOptions = {};
 
@@ -26,6 +27,20 @@ export const useOptionsStore = defineStore({
 
             return !isEqual(state.options, oldOptions) ? true : false;
         },
+
+        // ⚡️ Check if we have state in memory.
+        haveStateInMemory: (state) => {
+
+            if (typeof state.options === 'array') {
+
+                return state.options.length === 0 ? false : true;
+            }
+
+            if (typeof state.options === 'object') {
+
+                return Object.keys(state.options).length === 0 ? false : true;
+            }
+        },
     },
     actions: {
 
@@ -35,14 +50,27 @@ export const useOptionsStore = defineStore({
             apiFetch({
                 path: BASE_API_URL + '/get_options',
                 method: 'GET',
-            }).then((res) => {
-                const settingsValues = res.settings_values;
-                this.data = res.tabs;
-                this.options = settingsValues;
-                oldOptions = cloneDeep(settingsValues);
-                this.isLoading = false;
-                //console.log(res.tabs);
-            });
+            })
+                .then((res) => {
+                    const settingsValues = res.settings_values;
+                    this.data = res.tabs;
+                    this.options = settingsValues;
+                    oldOptions = cloneDeep(settingsValues);
+                })
+                .catch((err) => {
+
+                    console.log(err);
+
+                    ElMessage.error(({
+                        message: __("Something went wrong while fetching settings.", "addonify-quick-view"),
+                        offset: 50,
+                        duration: 10000,
+                    }));
+                })
+                .finally(() => {
+
+                    this.isLoading = false;
+                })
         },
 
         // ⚡️ Handle update options & map the values to the options object.
@@ -74,28 +102,42 @@ export const useOptionsStore = defineStore({
                 data: {
                     settings_values: payload
                 }
-            }).then((res) => {
+            })
+                .then((res) => {
 
-                this.isSaving = false; // Saving is compconsted here.
-                this.message = res.message; // Set the message to be displayed to the user.
+                    this.isSaving = false; // Saving is compconsted here.
+                    this.message = res.message; // Set the message to be displayed to the user.
 
-                if (res.success === true) {
-                    ElMessage.success(({
-                        message: this.message,
-                        offset: 50,
-                        duration: 3000,
-                    }));
-                } else {
+                    if (res.success === true) {
+                        ElMessage.success(({
+                            message: this.message,
+                            offset: 50,
+                            duration: 3000,
+                        }));
+                    } else {
+
+                        ElMessage.error(({
+                            message: this.message,
+                            offset: 50,
+                            duration: 3000,
+                        }));
+                    }
+
+                    let tempOptionsState = cloneDeep(this.options);
+                    this.options = {};
+                    this.options = cloneDeep(tempOptionsState);
+                    oldOptions = cloneDeep(this.options);
+                })
+                .catch((err) => {
+
+                    console.log(err);
 
                     ElMessage.error(({
-                        message: this.message,
+                        message: __("Something went wrong while updating settings.", "addonify-quick-view"),
                         offset: 50,
-                        duration: 3000,
+                        duration: 5000,
                     }));
-                }
-
-                this.fetchOptions(); // Fetch options again.
-            });
+                })
         },
     },
 });
