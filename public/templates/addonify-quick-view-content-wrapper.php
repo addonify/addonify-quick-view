f5d1d64a8dd37f056d5c936b6cfae8698eb0676d
Anuj Subedi <struggleage@gmail.com>
2023-08-08 13:08:06 +0545

    💜 OPTIMIZED: CLOSE BUTTON MARKUP.
    - 💜 OPTIMIZED: CLOSE BUTTON MARKUP.

--- public/templates/addonify-quick-view-content-wrapper.php
+++ public/templates/addonify-quick-view-content-wrapper.php
@@ -24,16 +24,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	data-open_animation="<?php echo esc_attr( addonify_quick_view_get_modal_animation('opening') ); ?>" 
 	data-close_animation="<?php echo esc_attr( addonify_quick_view_get_modal_animation('closing') ); ?>">
 	<div id="addonify-quick-view-modal" data-layout="default">
-		<?php if ( !addonify_quick_view_get_settings_fields_values( 'hide_modal_close_button' ) ) { ?>
-			<header id="adfy-qvm-header" class="adfy-qvm-header">
-				<button class="adfy-qv-button addonify-qvm-close-button">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-						<line x1="18" y1="6" x2="6" y2="18"></line>
-						<line x1="6" y1="6" x2="18" y2="18"></line>
-					</svg>
-				</button>
-			</header>
-		<?php } ?>
+		<button 
+		id="addonify-quick-view-modal-close"
+		class="adfy-qv-button"
+		data_hide-desktop="<?php echo esc_attr( addonify_quick_view_get_settings_fields_values( 'hide_modal_close_button' ) === '1' ? 'true' : 'false' ); ?>">
+			<span class="button-icon">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.0007 10.5865L16.9504 5.63672L18.3646 7.05093L13.4149 12.0007L18.3646 16.9504L16.9504 18.3646L12.0007 13.4149L7.05093 18.3646L5.63672 16.9504L10.5865 12.0007L5.63672 7.05093L7.05093 5.63672L12.0007 10.5865Z"></path></svg>
+			</span>
+			<span class="button-label mobile">
+				<?php echo esc_html( addonify_quick_view_get_settings_fields_values( 'mobile_close_button_label' ) ); ?>
+			</span>
+		</button>
 		<div class="adfy-quick-view-model-inner" id="adfy-quick-view-model-inner">
 			<div id="adfy-qvm-spinner">
 				<?php echo addonify_quick_view_escape_svg ( addonify_quick_view_get_spinner_icon( addonify_quick_view_get_settings_fields_values( 'spinner_icons' ) ) ); ?>
@@ -43,14 +44,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 				data-content_layout="<?php echo esc_attr( addonify_quick_view_get_settings_fields_values( 'modal_content_column_layout' ) ); ?>">
 			</section>
 		</div>
-		<footer id="adfy-qvm-footer" class="adfy-qvm-footer">
-			<button class="adfy-qv-button addonify-qvm-close-button">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-					<line x1="18" y1="6" x2="6" y2="18"></line>
-					<line x1="6" y1="6" x2="18" y2="18"></line>
-				</svg>
-				<?php echo esc_html( addonify_quick_view_get_settings_fields_values( 'mobile_close_button_label' ) ); ?>
-			</button>
-		</footer>
 	</div>
 </aside>
