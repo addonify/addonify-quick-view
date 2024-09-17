1b778d7c5e2573dd2e70f1035c37b09a211dcb86
Anuj Subedi <struggleage@gmail.com>
2024-09-17 14:13:28 +0545

    :fire: Added: new vite app with tailwind.
    - :fire: Added: new vite app with tailwind.

--- admin/class-addonify-quick-view-admin.php
+++ admin/class-addonify-quick-view-admin.php
@@ -45,7 +45,7 @@ class Addonify_Quick_View_Admin {
 	 * @access   private
 	 * @var      string    $version    The current version of this plugin.
 	 */
-	private $settings_page_slug = 'addonify_quick_view';
+	private $settings_page_slug = 'addonify-quick-view';
 
 
 	/**
@@ -87,53 +87,53 @@ class Addonify_Quick_View_Admin {
 	 */
 	public function enqueue_scripts() {
 
-		wp_register_script(
-			"{$this->plugin_name}-manifest",
-			plugin_dir_url( __FILE__ ) . 'assets/js/manifest.js',
-			null,
-			$this->version,
-			true
-		);
+		//wp_register_script(
+		//	"{$this->plugin_name}-manifest",
+		//	plugin_dir_url( __FILE__ ) . 'assets/js/manifest.js',
+		//	null,
+		//	$this->version,
+		//	true
+		//);
 
-		wp_register_script(
-			"{$this->plugin_name}-vendor",
-			plugin_dir_url( __FILE__ ) . 'assets/js/vendor.js',
-			array( "{$this->plugin_name}-manifest" ),
-			$this->version,
-			true
-		);
+		//wp_register_script(
+		//	"{$this->plugin_name}-vendor",
+		//	plugin_dir_url( __FILE__ ) . 'assets/js/vendor.js',
+		//	array( "{$this->plugin_name}-manifest" ),
+		//	$this->version,
+		//	true
+		//);
 
-		wp_register_script(
-			"{$this->plugin_name}-main",
-			plugin_dir_url( __FILE__ ) . 'assets/js/main.js',
-			array( 'lodash', "{$this->plugin_name}-vendor", 'wp-i18n', 'wp-api-fetch' ),
-			$this->version,
-			true
-		);
+		//wp_register_script(
+		//	"{$this->plugin_name}-main",
+		//	plugin_dir_url( __FILE__ ) . 'assets/js/main.js',
+		//	array( 'lodash', "{$this->plugin_name}-vendor", 'wp-i18n', 'wp-api-fetch' ),
+		//	$this->version,
+		//	true
+		//);
 
-		if (
-			isset( $_GET['page'] ) && // phpcs:ignore
-			$_GET['page'] === $this->settings_page_slug // phpcs:ignore
-		) {
-			wp_enqueue_script( "{$this->plugin_name}-manifest" );
+		//if (
+		//	isset( $_GET['page'] ) && // phpcs:ignore
+		//	$_GET['page'] === $this->settings_page_slug // phpcs:ignore
+		//) {
+		//	wp_enqueue_script( "{$this->plugin_name}-manifest" );
 
-			wp_enqueue_script( "{$this->plugin_name}-vendor" );
+		//	wp_enqueue_script( "{$this->plugin_name}-vendor" );
 
-			wp_enqueue_script( "{$this->plugin_name}-main" );
+		//	wp_enqueue_script( "{$this->plugin_name}-main" );
 
-			wp_localize_script(
-				"{$this->plugin_name}-main",
-				'adfy_wp_locolizer',
-				array(
-					'admin_url'      => esc_url( admin_url( '/' ) ),
-					'ajax_url'       => esc_url( admin_url( 'admin-ajax.php' ) ),
-					'rest_namespace' => 'addonify_quick_view_options_api',
-					'version_number' => $this->version,
-				)
-			);
-		}
+		//	wp_localize_script(
+		//		"{$this->plugin_name}-main",
+		//		'adfy_wp_locolizer',
+		//		array(
+		//			'admin_url'      => esc_url( admin_url( '/' ) ),
+		//			'ajax_url'       => esc_url( admin_url( 'admin-ajax.php' ) ),
+		//			'rest_namespace' => 'addonify_quick_view_options_api',
+		//			'version_number' => $this->version,
+		//		)
+		//	);
+		//}
 
-		wp_set_script_translations( "{$this->plugin_name}-main", $this->plugin_name );
+		// wp_set_script_translations( "{$this->plugin_name}-main", $this->plugin_name );
 	}
 
 
@@ -234,7 +234,7 @@ class Addonify_Quick_View_Admin {
 	 */
 	public function get_settings_screen_contents() {
 		?>
-		<div id="___adfy-quickview-app___"></div>
+		<div id="addonify-quick-view-app"></div>
 		<?php
 	}
 }
