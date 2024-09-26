a842a15614e9df73d19b59d0c07f2a6cccfac19b
Anuj Subedi <struggleage@gmail.com>
2024-09-26 12:48:10 +0545

    :package: Added:  icon to the modal tab.
    - :package: Added:  icon to the modal tab.

--- includes/functions/fields/general-v2.php
+++ includes/functions/fields/general-v2.php
@@ -35,13 +35,13 @@ if ( ! function_exists( 'addonify_quick_view_general_fields' ) ) {
 			'addonify_quick_view_general_fields',
 			array(
 				'enable_quick_view'                   => array(
-					'label'       => esc_html__( 'Enable Quick View', 'addonify-quick-view' ),
+					'label'       => esc_html__( 'Enable quick view', 'addonify-quick-view' ),
 					'description' => esc_html__( 'If disabled, quick view features will be disabled completely.', 'addonify-quick-view' ),
 					'type'        => 'switch',
 					'value'       => addonify_quick_view_get_option( 'enable_quick_view' ),
 				),
 				'disable_quick_view_on_mobile_device' => array(
-					'label'       => esc_html__( 'Disable on Mobile Devices', 'addonify-quick-view' ),
+					'label'       => esc_html__( 'Disable on mobile devices', 'addonify-quick-view' ),
 					'description' => esc_html__( 'If enabled, quick view will be disabled on mobile devices.', 'addonify-quick-view' ),
 					'type'        => 'switch',
 					'value'       => addonify_quick_view_get_option( 'disable_quick_view_on_mobile_device' ),
