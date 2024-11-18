eb683aa69cabe67e578f6972523ebdf891b71849
lalman lama <lalman.creamcode@gmail.com>
2024-11-18 16:00:15 +0545

    phpcs
    -phpcs

--- public/templates/addonify-quick-view-content.php
+++ public/templates/addonify-quick-view-content.php
@@ -19,14 +19,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
-
-do_action( 'addoify_quick_view_before_single_content', $product );
 $modal_post_class  = 'product';
 $modal_box_content = unserialize( addonify_quick_view_get_option( 'modal_box_content' ) ); // phpcs:ignore
 
 if ( is_array( $modal_box_content ) && ! in_array( 'image', $modal_box_content, true ) ) {
 	$modal_post_class .= ' aqv-no-product-image';
 }
+
+do_action( 'addoify_quick_view_before_single_content', $product );
 ?>
 <div class="woocommerce single-product">
 	<div id="product-<?php echo esc_attr( $product->get_id() ); ?>" <?php post_class( $modal_post_class ); ?>>
